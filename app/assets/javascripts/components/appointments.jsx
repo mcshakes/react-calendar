3fe6c39f2cdcd36430695645d66e3e28761ff5af
Edward McCarthy <efmccarthy26@gmail.com>
2017-07-07 13:33:29 -0600

    form seems fine, errors talking about list ID preference

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,9 +1,18 @@
 var Appointments = React.createClass({
+  // managing the state of the form and the list in this file, since this is the parent that houses them
+  getInitialState: function() {
+    return {
+      appointments: this.props.appointments,
+      input_title: "Team standup",
+      input_appt_time: "Tomorrow at 9am"
+    }
+  },
+
   render: function() {
     return (
         <div>
-          <AppointmentForm />
-          <AppointmentsList appointments={this.props.appointments} />
+          <AppointmentForm input_title={this.state.input_title} input_appt_time={this.state.input_appt_time}/>
+          <AppointmentsList appointments={this.state.appointments} />
         </div>
     )
   }
